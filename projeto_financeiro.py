1e0889b503c61adabb19c35b86ff8b93970cb480
Eduardo <eduardocapelinf@gmail.com>
2025-06-07 13:03:26 -0300

    build: leitura pro excel

--- projeto_financeiro.py
+++ projeto_financeiro.py
@@ -5,10 +5,9 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
+arquivo = "ResumoFinanceiro.xlsx"
 api_key = os.getenv("api_key")
-
 url = f"https://api.telegram.org/bot{api_key}/getUpdates"
-
 response = requests.get(url)
 
 data = response.json()
@@ -27,4 +26,27 @@ for item in dataResult:
 
 df = pd.DataFrame(dados)
 
+#Filtro pra apagar as linhas testes
+df = df[df["message_id"] >= 7]
 print(df)
+#
+
+df["date"] = pd.to_datetime(df["date"], unit="s")
+
+def limpar_mensagem(txt:str):
+    valor = float(txt.split(" ")[0].replace(",", "."))
+    descricao = txt.split(" ", 1)[1].rsplit(" ", 1)[0]
+    forma_pagamento = txt.split(" ")[-1].upper()
+    return valor, descricao, forma_pagamento
+
+df["valor"] = df["text"].apply(lambda x: limpar_mensagem(x)[0])
+df["descricao"] = df["text"].apply(lambda x: limpar_mensagem(x)[1])
+df["forma_pagamento"] = df["text"].apply(lambda x: limpar_mensagem(x)[2])
+
+df = df.drop("text", axis=1)
+
+df.to_excel(f"Projeto_Financeiro/{arquivo}", sheet_name="Base", index=False, )
+
+df_atual = pd.read_excel(f"Projeto_Financeiro/{arquivo}")
+
+print(df_atual)
